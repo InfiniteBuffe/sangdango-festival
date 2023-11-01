003e4bac921cc774c0b7d59a26dedddd26d43a0d
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2023-11-01 11:27:38 +0900

    홈 화면 디자인 추가

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/pages/Home/Home.module.css'
 import TopSlideBanner from '@/components/TopSlideBanner'
+import Snowfall from 'react-snowfall'
+import React from "react"
+React.useLayoutEffect = React.useEffect
 
 export default function Home() {
   return (
@@ -9,7 +12,29 @@ export default function Home() {
       <Head>
         <title>2023 상당고 용천제</title>
       </Head>
+      <Snowfall
+        color='white'
+        snowflakeCount={25}
+      />
       <TopSlideBanner />
+      <Image
+        src="sangdang.jpg"
+        width={0}
+        height={0}
+        style={{
+          width: '100%',
+          minWidth: '800px',
+          height: 'auto',
+          minHeight: '300px',
+          filter: 'blur(40px) brightness(30%) opacity(80%)',
+          textAlign: 'center',
+          position: 'absolute',
+          top: '30px',
+          userSelect: 'none',
+          '-webkit-user-drag': 'none'
+        }}
+      loader={({ src }) => { return `https://cdn.sangdang.kr/${src}` }}
+      />
     </>
   )
 }
