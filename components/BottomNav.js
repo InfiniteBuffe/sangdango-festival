5a1d84594d7fd404b091ce0b986345afb666d238
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2023-11-08 01:14:19 +0900

    bottomNav에 예약 탭 추가

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -1,6 +1,15 @@
 import styles from '@/styles/components/BottomNav/BottomNav.module.css'
 import { useRouter } from 'next/router'
-import { IoHomeOutline, IoHomeSharp, IoBookOutline, IoBookSharp, IoPaperPlaneOutline, IoPaperPlaneSharp } from 'react-icons/io5'
+import {
+    IoHomeOutline,
+    IoHome,
+    IoBookOutline,
+    IoBook, 
+    IoPaperPlaneOutline,
+    IoPaperPlane,
+    IoBookmarkOutline,
+    IoBookmark
+} from 'react-icons/io5'
 
 const BottomNav = (props) => {
 
@@ -12,26 +21,34 @@ const BottomNav = (props) => {
             <div className={styles.space} />
             <div className={styles.nav}>
                 <div className={styles.box}>
+                    <div className={styles.menu} onClick={() => router.push('/')}>
+                        <div>
+                            <div className={styles.icon}>
+                                {(now_tab == 'home') ? (<IoHome size={20} />) : (<IoHomeOutline size={20} />)}
+                            </div>
+                            <div className={styles.name}>홈</div>
+                        </div>
+                    </div>
                     <div className={styles.menu} onClick={() => router.push('/brochure')}>
                         <div>
                             <div className={styles.icon}>
-                                {(now_tab == 'brochure') ? (<IoBookSharp size={20} />) : (<IoBookOutline size={20} />)}
+                                {(now_tab == 'brochure') ? (<IoBook size={20} />) : (<IoBookOutline size={20} />)}
                             </div>
                             <div className={styles.name}>가이드북</div>
                         </div>
                     </div>
-                    <div className={styles.menu} onClick={() => router.push('/')}>
+                    <div className={styles.menu} onClick={() => router.push('/booking')}>
                         <div>
                             <div className={styles.icon}>
-                                {(now_tab == 'home') ? (<IoHomeSharp size={20} />) : (<IoHomeOutline size={20} />)}
+                                {(now_tab == 'booking') ? (<IoBookmark size={20} />) : (<IoBookmarkOutline size={20} />)}
                             </div>
-                            <div className={styles.name}>홈</div>
+                            <div className={styles.name}>예약</div>
                         </div>
                     </div>
                     <div className={styles.menu} onClick={() => router.push('/vote')}>
                         <div>
                             <div className={styles.icon}>
-                                {(now_tab == 'vote') ? (<IoPaperPlaneSharp size={20} />) : (<IoPaperPlaneOutline size={20} />)}
+                                {(now_tab == 'vote') ? (<IoPaperPlane size={20} />) : (<IoPaperPlaneOutline size={20} />)}
                             </div>
                             <div className={styles.name}>투표</div>
                         </div>
---
+++ pages/booking.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/pages/Booking/Booking.module.css'
+
+const Booking = () =>{ 
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Booking
